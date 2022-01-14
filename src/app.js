a607f380c4d0ebaf0df3e40d80032e769c01e23c
unknown <sazwerling@gmail.com>
2022-01-14 16:52:24 -0600

    serving HTML and JSON

--- src/app.js
+++ src/app.js
@@ -2,24 +2,30 @@ const express = require('express'); // returns a function
 
 const app = express();
 
-app.get('', (req, res) => {
-    res.send('Hello express!')
+app.get('', (req, res) => {              //configures what app should do when someone attempts to 
+    res.send('<h1>weather</h1>')           //get resource at a specific url
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help page!')
+    res.send({
+        myName: 'Slim',
+        age: 46
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.send('THE ABOUT PAGE')
+    res.send('<h1>The About Page</h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('WEATHER PAGE')
+    res.send({
+        forecast: 'lots of rain',
+        location: 'your moms house'
+    })
 })
 
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(3000, () => {   //second argt is a callback function that runs when the server is up and running
+    console.log('up and running')
 })
