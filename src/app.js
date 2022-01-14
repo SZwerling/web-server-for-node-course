79ceeae1836d41bb9a82453ada64496e0a3d8f75
unknown <sazwerling@gmail.com>
2022-01-14 17:59:01 -0600

    serving up static assets

--- src/app.js
+++ src/app.js
@@ -1,21 +1,11 @@
+const path = require('path')
 const express = require('express'); // returns a function
 
+
 const app = express();
+app.use(express.static(path.join(__dirname, '../public')))
 
-app.get('', (req, res) => {              //configures what app should do when someone attempts to 
-    res.send('<h1>weather</h1>')           //get resource at a specific url
-})
 
-app.get('/help', (req, res) => {
-    res.send({
-        myName: 'Slim',
-        age: 46
-    })
-})
-
-app.get('/about', (req, res) => {
-    res.send('<h1>The About Page</h1>')
-})
 
 app.get('/weather', (req, res) => {
     res.send({
