d66d5258d45d194d19874ed7486eb8ed83d8bce0
unknown <sazwerling@gmail.com>
2022-01-17 21:27:46 -0600

    dynamic pages w templating

--- src/app.js
+++ src/app.js
@@ -5,6 +5,28 @@ const express = require('express'); // returns a function
 const app = express();
 app.use(express.static(path.join(__dirname, '../public')))
 
+app.set('view engine', 'hbs')
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'weather app',
+        name: 'Seth Zwerling'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'about a dog',
+        name: 'His name was Mr. Zwerling'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'We are here to help you.'
+    })
+})
+
 
 
 app.get('/weather', (req, res) => {
