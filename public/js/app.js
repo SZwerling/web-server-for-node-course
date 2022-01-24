c9a820573ec0441d2ba9e550c9c443ec31537854
unknown <sazwerling@gmail.com>
2022-01-24 10:39:48 -0600

    browser req w fetch

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,14 @@
 console.log('client-side js file is loaded')
+
+
+
+fetch('http://localhost:3000/weather?address=los angeles,California').then((response) => {
+    response.json().then((data) => {
+        if(data.error){
+            console.log(data.error)
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
